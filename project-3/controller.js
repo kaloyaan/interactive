f835d48055c6b347c4518005b08deab78f7fb9c7
Kaloyan Kolev <42871424+kaloyaan@users.noreply.github.com>
2023-04-18 15:27:33 -0400

    gyro

--- project-3/controller.js
+++ project-3/controller.js
@@ -7,7 +7,6 @@ const images = [
   ["blue-1.jpeg", "blue-2.jpeg", "blue-3.jpeg", "blue-4.jpeg", "blue-5.jpeg"],
 ];
 
-
 let isDragging = false;
 
 // Add listeners for mouse and touch events
@@ -31,8 +30,11 @@ document.addEventListener("touchend", () => {
     isDragging = false;
 });
 
+// Add listener for gyro controls
+window.addEventListener("deviceorientation", handleOrientation);
+
 function handleMove(event) {
-    if (isDragging) {
+    if (isDragging || event.type === 'deviceorientation') {
         const containerRect = image.getBoundingClientRect();
         const containerWidth = containerRect.width;
         const containerHeight = containerRect.height;
@@ -41,11 +43,14 @@ function handleMove(event) {
         const circleWidth = circleRect.width;
         const circleHeight = circleRect.height;
 
-        // Calculate the position of the circle within the container
+        // Calculate the position of the circle within the container based on the event type
         let x, y;
         if (event.touches) {
             x = event.touches[0].clientX - containerRect.left - (circleWidth / 2);
             y = event.touches[0].clientY - containerRect.top - (circleHeight / 2);
+        } else if (event.type === 'deviceorientation') {
+            x = containerWidth * ((event.gamma + 90) / 180);
+            y = containerHeight * ((event.beta + 90) / 180);
         } else {
             x = event.clientX - containerRect.left - (circleWidth / 2);
             y = event.clientY - containerRect.top - (circleHeight / 2);
